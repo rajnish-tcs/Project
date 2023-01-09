a7380fc54d73345f0b2f6a012809e0e754e617a4
rajnish-tcs <robinuit123@gmail.com>
2023-01-08 22:01:40 -0800

    Second commit

--- FirstProject/src/in/ineuoron/main/Test.java
+++ FirstProject/src/in/ineuoron/main/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first project");
+		int [] arr = {10,20,30,40};
+		for(int elem : arr) {
+			System.out.println(elem);
+		}
 	}
 
 }
