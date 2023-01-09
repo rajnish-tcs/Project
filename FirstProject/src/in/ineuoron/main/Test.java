ceb890841121f861af2c0fa1907431e25edc24e6
rajnish-tcs <robinuit123@gmail.com>
2023-01-08 22:04:06 -0800

    Third Changes

--- FirstProject/src/in/ineuoron/main/Test.java
+++ FirstProject/src/in/ineuoron/main/Test.java
@@ -5,10 +5,16 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first project");
+		
+		
 		int [] arr = {10,20,30,40};
 		for(int elem : arr) {
 			System.out.println(elem);
 		}
+		
+		StringBuffer sb = new StringBuffer("sachin");
+		sb.append("Tendulkar");
+		System.out.println(sb);
 	}
 
 }
